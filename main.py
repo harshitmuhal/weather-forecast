91d68e4d25f4dfcba221ba87f7805cfb43c2daaf
Harshit Muhal <42070734+harshitmuhal@users.noreply.github.com>
2019-02-20 19:16:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def weather_details(city_name):
+    api_key = "8a36d39fa8339df10e5711b9e79e8311"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    comp_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(comp_url)
+    x = response.json()
+    if x["cod"] != "404":
+        print("Temperature (in kelvin unit) = ", x['main']['temp'], "\nMinimum Temperature (in kelvin unit) = ",
+              x['main']['temp_min'],
+              "\nMaximum Temperature (in kelvin unit) = ", x['main']['temp_max'],
+              "\nAtmospheric pressure (in hPa unit) = ",
+              x['main']['pressure'], "\nHumidity (in percentage) =",
+              x['main']['humidity'], "\nVisibility (in metre unit)=", x['visibility'], "\nWind speed(in km/hr unit) =",
+              x['wind']['speed'],
+              "\nDescription = ", x['weather'][0]["description"])
+    else:
+        print('city not found')
+
+
+city = input("enter city name")
+weather_details(city)
